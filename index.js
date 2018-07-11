d9c541d9bf28339c375788e62bdb21466de5322d
DESKTOP-7PDFHTN\mytac <my_tac@126.com>
2018-07-11 17:06:13 +0800

    style: eslint

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const fs=require('fs')
+const fs = require('fs');
 const prompts = require('prompts');
 
-(async function(){
-    const options = [
-        {
-            type: 'text',
-            name: 'theme',
-            message: `Please input the theme name`,
-            initial: 'light',
-            format: v => `${v}`
-        },
-        {
-            type: 'confirm',
-            name: 'isChinese',
-            initial: true,
-            message: 'Is the default language Chinese?' 
-        },
-    ];
+(async function () {
+  const options = [
+    {
+      type: 'text',
+      name: 'theme',
+      message: 'Please input the theme name',
+      initial: 'light',
+      format: v => `${v}`,
+    },
+    {
+      type: 'confirm',
+      name: 'isChinese',
+      initial: true,
+      message: 'Is the default language Chinese?',
+    },
+  ];
 
-    console.log('Initial configuration...')
-    const answers = JSON.stringify(await prompts(options));
+  console.log('Initial configuration...');
+  const answers = JSON.stringify(await prompts(options));
 
-    fs.writeFile('resume-config.json', answers, ()=>{
-        console.log(answers);
-    })
-})();
+  fs.writeFile('resume-config.json', answers, () => {
+    console.log(answers);
+  });
+}());

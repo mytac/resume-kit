1bf7775993f60f8b06543edf7553d2a996426c67
DESKTOP-7PDFHTN\mytac <my_tac@126.com>
2018-07-12 18:07:42 +0800

    feat: changelog test

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-const express=require('express')
-const ejs=require('ejs')
-const app=express()
-const c = require('child_process');
-const portIsOccupied=require('./utils/portIsOccupied')
-app.set('view engine','ejs')
-app.use('/assets',express.static('assets'))
+const express = require('express');
+// const ejs = require('ejs');
 
-const data=require('./config.json')
+const app = express();
+// const c = require('child_process');
+const portIsOccupied = require('./utils/portIsOccupied');
 
-app.get('/',(req,res)=>{
-    // console.log('res',res)
-    res.render('index',data)
-})
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
-app.use(function(req, res, next) {
-    res.status(404).send('Sorry cant find that!');
+const data = require('./config.json');
+
+app.get('/', (req, res) => {
+  // console.log('res',res)
+  res.render('index', data);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Sorry cant find that!');
+});
+
+portIsOccupied(3000).then((port) => {
+  app.listen(port, () => {
+    console.log(`start http://localhost:${port}`);
+    // c.exec(`start http://localhost:${port}`)
   });
-
-portIsOccupied(3000).then(port=>{
-    app.listen(port,()=>{
-        console.log(`start http://localhost:${port}`)
-        // c.exec(`start http://localhost:${port}`)
-    })
-})
+});

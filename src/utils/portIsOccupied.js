b1ee9fa94d468e5cc2ddeb4fe8fbde300d702d83
mytac <my_tac@126.com>
2018-08-03 18:43:44 +0800

    fix: temp

--- src/utils/portIsOccupied.js
+++ src/utils/portIsOccupied.js
@@ -1,26 +1,24 @@
-const net=require('net')
- 
-function portIsOccupied (port){
-    const server=net.createServer().listen(port)
-    return new Promise((resolve,reject)=>{
-        server.on('listening',()=>{
-            console.log(`the server is runnint on port ${port}`)
-            server.close()
-            resolve(port)
-        })
-    
-        server.on('error',(err)=>{
-            if(err.code==='EADDRINUSE'){
-                resolve(portIsOccupied(port+1))
-                console.log(`this port ${port} is occupied.try another.`)
-            }else{
-                reject(err)
-            }
-        })
-    })
-    
-} 
+const net = require('net');
+
+function portIsOccupied(port) {
+  const server = net.createServer().listen(port);
+  return new Promise((resolve, reject) => {
+    server.on('listening', () => {
+      console.log(`the server is runnint on port ${port}`);
+      server.close();
+      resolve(port);
+    });
+
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        resolve(portIsOccupied(port + 1));
+        console.log(`this port ${port} is occupied.try another.`);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
 
 
-module.exports=portIsOccupied
-
+module.exports = portIsOccupied;
